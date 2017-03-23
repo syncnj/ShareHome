2df8605b1585f30f834898ff021091ebe086aa9a
AaronHProgramming <aaronnhager@yahoo.com>
2017-03-23 14:44:19 -0500

    Added overflow menu to action bar. Should be replaced with Hamburger Menu.

--- AndroidShareHome/app/src/main/java/sharehome/com/myapplication/MainActivity.java
+++ AndroidShareHome/app/src/main/java/sharehome/com/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package sharehome.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.content.Intent;
 
@@ -24,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
     public void launchProfileActivity(View view) {
         Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
