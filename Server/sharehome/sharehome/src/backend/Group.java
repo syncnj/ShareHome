b3e8a6866ff0b3efe831af4bfe8c246d123f7db9
yifeifeng <ghoulknight95@gmail.com>
2017-04-11 03:20:35 -0500

    Add files via upload

---
+++ Server/sharehome/sharehome/src/backend/Group.java
@@ -0,0 +1,34 @@
+package backend;
+
+//import com.backendless.*;
+public class Group {
+	private String groupName;
+	private String ownerId;
+	private String objectId; // group unique id
+	private String teamMembersList;
+	
+	public String getObjectId() {
+	    return objectId;
+	  }
+	public String getGroupName(){
+		return groupName;
+	}
+	public String getOwnerId(){
+		return ownerId;
+	}
+	public String getTeamMembersList(){
+		return teamMembersList;
+	}
+	public void setGroupName( String groupName ) {
+	    this.groupName= groupName;
+	  }
+	public void setOwnerId( String ownerId ) {
+	    this.ownerId= ownerId;
+	  }
+	public void setObjectId (String objectId ) {
+	    this.objectId= objectId;
+	  }
+	public void setTeamMembersList (String teamMembersList){
+		this.teamMembersList = teamMembersList;
+	}
+}
---
+++ Server/sharehome/sharehome/src/backend/GroupService.java
@@ -0,0 +1,15 @@
+package backend;
+
+import com.backendless.*;
+import com.backendless.servercode.IBackendlessService;
+
+public class GroupService implements IBackendlessService{
+
+	public void createNewGroup(String groupName, String ownerId){
+		Group group = new Group();
+		group.setGroupName(groupName);
+		group.setOwnerId(ownerId);
+		group.setTeamMembersList(null);
+		Group savedGroup = Backendless.Persistence.save( group );
+	}
+}
