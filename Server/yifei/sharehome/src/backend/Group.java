a12019e294447bb542d968e6c2b0e0f1765e5774
yifeifeng <ghoulknight95@gmail.com>
2017-04-13 01:50:01 -0500

    update and fixbug for group service

--- Server/yifei/sharehome/src/backend/Group.java
+++ Server/yifei/sharehome/src/backend/Group.java
@@ -6,7 +6,12 @@ public class Group {
 	private String ownerId;
 	private String objectId; // group unique id
 	private String teamMembersList;
+	private String leaderId;
 	
+	
+	public String getLeaderId(){
+		return leaderId;
+	}
 	public String getObjectId() {
 	    return objectId;
 	  }
@@ -22,13 +27,22 @@ public class Group {
 	public void setGroupName( String groupName ) {
 	    this.groupName= groupName;
 	  }
-	public void setOwnerId( String ownerId ) {
-	    this.ownerId= ownerId;
-	  }
-	public void setObjectId (String objectId ) {
-	    this.objectId= objectId;
-	  }
-	public void setTeamMembersList (String teamMembersList){
-		this.teamMembersList += teamMembersList +",";
+//	public void setOwnerId( String ownerId ) {
+//	    this.ownerId= ownerId;
+//	  }
+//	public void setObjectId (String objectId ) {
+//	    this.objectId= objectId;
+//	  }
+	public void setLeaderId(String newLeaderId){
+		this.leaderId= newLeaderId;
+	}
+	public void addTeamMember (String teamMember){
+		this.teamMembersList += teamMember +",";
+	}
+	public void setTeamMembersList(String newTeamMembersList){
+		this.teamMembersList = newTeamMembersList;
+	}
+	public void concatMembersLists(String teamMembersList){
+		this.teamMembersList += teamMembersList;
 	}
 }
--- Server/yifei/sharehome/src/backend/GroupService.java
+++ Server/yifei/sharehome/src/backend/GroupService.java
@@ -6,22 +6,78 @@ import com.backendless.servercode.IBackendlessService;
 
 public class GroupService implements IBackendlessService{
 
-	public void createNewGroup(String groupName, String ownerId){
+	public void createNewGroup(String groupName, String leaderId){
+		if(groupName == null || ownerId == null){
+			throw new IllegalArgumentException("Missing input message!!!");
+		}
 		Group group = new Group();
 		group.setGroupName(groupName);
-		group.setOwnerId(ownerId);
-		group.setTeamMembersList(null);
+		group.setLeaderId(leaderId);
+		group.addTeamMember(leaderId);
 		Group savedGroup = Backendless.Persistence.save( group );
 	}
 	
-	public boolean addNewMember(String newGroupMemberId,String groupId){
+	public boolean addMember(String newGroupMemberId,String groupId){
+		if(newGroupMemberId == null || groupId == null){
+			throw new IllegalArgumentException("Missing input message!!!");
+		}
 		Group group = Backendless.Persistence.of(Group.class).findById(groupId);
 		if(group == null){
 			return false;
 		}
 		else{
-			group.setTeamMembersList(newGroupMemberId);
+			group.addTeamMember(newGroupMemberId);
 			return true;
 		}
 	}
+	 public String getMemberList(String groupId){
+		 if( groupId == null){
+				throw new IllegalArgumentException("Missing input message!!!");
+			}
+		 Group group = Backendless.Persistence.of(Group.class).findById(groupId);
+			if(group == null){
+				return null;
+			}
+			else{
+			
+				return group.getTeamMembersList();
+			}
+	 }
+	
+	 
+	public boolean deleteMember(String groupId, String memberId) throws Exception{
+		if(groupId== null || memberId == null){
+			throw new IllegalArgumentException("Missing input message!!!");
+		}
+		 Group group = Backendless.Persistence.of(Group.class).findById(groupId);
+			if(group == null){
+				return false;
+			}
+			else{
+				String memberList =group.getTeamMembersList();
+				if(memberList.contains(memberId)){
+					int indexOfFirstChar = memberList.indexOf(memberId);
+					if(indexOfFirstChar == 0){  // member is owner
+						throw new Exception("owner can not be deleted!!!");
+					}
+					else if (indexOfFirstChar + memberId.length() + 1 == memberList.length()){
+						// member is last user in group
+						
+						
+						group.setTeamMembersList(memberList.substring(0, indexOfFirstChar));
+						
+						return  true;
+					}
+					else{
+						// member is neither owner nor the last user
+						String beforeDeletedMember = group.getTeamMembersList().substring(0, indexOfFirstChar);
+						String afterDeletedMember = group.getTeamMembersList().substring((indexOfFirstChar+memberId.length()+1), group.getTeamMembersList().length());
+						group.setTeamMembersList(beforeDeletedMember+afterDeletedMember);
+						return true;
+					}
+				}
+				return false ;
+			}
+	}
+	
 }
