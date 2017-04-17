036a051b6a5f399dc6363800bc0d80a90dfa5294
Yi Shen <yblurobe@gmail.com>
2017-04-17 03:23:54 -0500

    Add code runner for timed task

---
+++ Server/ShareHome-Timer/src/com/sharehome/timers/TaskManagerTimer.java
@@ -0,0 +1,41 @@
+package com.sharehome.timers;
+
+import com.backendless.Backendless;
+import com.backendless.persistence.BackendlessDataQuery;
+import com.backendless.servercode.annotation.BackendlessTimer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+* TaskManagerTimer is a timer.
+* It is executed according to the schedule defined in Backendless Console. The
+* class becomes a timer by extending the TimerExtender class. The information
+* about the timer, its name, schedule, expiration date/time is configured in
+* the special annotation - BackendlessTimer. The annotation contains a JSON
+* object which describes all properties of the timer.
+*/
+@BackendlessTimer("{'startDate':1492411200000,'frequency':{'schedule':'custom','repeat':{'every':3600}},'timername':'TaskManager'}")
+public class TaskManagerTimer extends com.backendless.servercode.extension.TimerExtender
+{
+    
+  @Override
+  public void execute( String appVersionId ) throws Exception
+  {
+    // add your code here
+    // duration is the days between each day
+    String whereClause = "startTime before " + System.currentTimeMillis();
+    BackendlessDataQuery dataQuery = new BackendlessDataQuery();
+    dataQuery.setWhereClause( whereClause );
+
+    List<Map> tasksDue = Backendless.Persistence.of( "Task" ).find( dataQuery ).getCurrentPage();
+
+    //BackendlessCollection<Map> allTasks = Backendless.Persistence.of( "Task" ).find();
+    for ( Map task: tasksDue ){
+        
+      System.out.print(task);
+    }
+  }
+    
+}
+        
