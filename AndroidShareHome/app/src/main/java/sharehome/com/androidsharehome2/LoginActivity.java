e4ff0c0a63241c026d4af0202a0eab127f2ea17e
tzrtzr000 <tzrtzr000@gmail.com>
2017-11-04 06:32:06 -0500

    login succesful

--- AndroidShareHome/app/src/main/java/sharehome/com/androidsharehome2/LoginActivity.java
+++ AndroidShareHome/app/src/main/java/sharehome/com/androidsharehome2/LoginActivity.java
@@ -1,7 +1,9 @@
 package sharehome.com.androidsharehome2;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -12,6 +14,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ForgotPasswordContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.NewPasswordContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.exceptions.CognitoNotAuthorizedException;
+
+import java.util.Locale;
+
 import butterknife.ButterKnife;
 import butterknife.BindView;
 
@@ -19,11 +35,21 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
-    @BindView(R.id.input_email) EditText _emailText;
+    @BindView(R.id.input_username) EditText _usernameText;
     @BindView(R.id.input_password) EditText _passwordText;
     @BindView(R.id.btn_login) Button _loginButton;
     @BindView(R.id.link_signup) TextView _signupLink;
 
+    //Continuations
+    private MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation;
+    private ForgotPasswordContinuation forgotPasswordContinuation;
+    private NewPasswordContinuation newPasswordContinuation;
+
+    // User Details
+    private String username;
+    private String password;
+    private ProgressDialog progressDialog;
+    private AlertDialog userDialog;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,11 +75,16 @@ public class LoginActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+
+        // Initialize application
+        AppHelper.init(getApplicationContext());
+        findCurrent();
     }
 
     public void login() {
         Log.d(TAG, "Login");
-
+        username = _usernameText.getText().toString();
+        password = _passwordText.getText().toString();
         if (!validate()) {
             onLoginFailed();
             return;
@@ -61,28 +92,63 @@ public class LoginActivity extends AppCompatActivity {
 
         _loginButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+        progressDialog = new ProgressDialog(LoginActivity.this,
                 R.style.AppTheme_Dark_Dialog);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Authenticating...");
         progressDialog.show();
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+        username = _usernameText.getText().toString();
+        password = _passwordText.getText().toString();
 
         // TODO: Implement authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        AppHelper.setUser(username);
+        AppHelper.getPool().getUser(username).getSessionInBackground(authenticationHandler);
+//        new android.os.Handler().postDelayed(
+//                new Runnable() {
+//                    public void run() {
+//                        // On complete call either onLoginSuccess or onLoginFailed
+//                        onLoginSuccess();
+//                        // onLoginFailed();
+//                        progressDialog.dismiss();
+//                    }
+//                }, 3000);
     }
 
+    private void findCurrent() {
+        CognitoUser user = AppHelper.getPool().getCurrentUser();
+        username = user.getUserId();
+        if(username != null) {
+            AppHelper.setUser(username);
+            _usernameText.setText(user.getUserId());
+            user.getSessionInBackground(authenticationHandler);
+        }
+    }
+
+    private void showDialogMessage(String title, String body) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title).setMessage(body).setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try {
+                    userDialog.dismiss();
+                } catch (Exception e) {
+                    //
+                }
+            }
+        });
+        userDialog = builder.create();
+        userDialog.show();
+    }
+
+    private void closeWaitDialog() {
+        try {
+            progressDialog.dismiss();
+        }
+        catch (Exception e) {
+            //
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -103,31 +169,31 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginSuccess() {
+        Toast.makeText(getBaseContext(), "Login Successful!", Toast.LENGTH_LONG).show();
         _loginButton.setEnabled(true);
         finish();
     }
 
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
         _loginButton.setEnabled(true);
     }
 
     public boolean validate() {
         boolean valid = true;
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+//        username = _usernameText.getText().toString();
+//        password = _passwordText.getText().toString();
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+        if (username.isEmpty()) {
+            _usernameText.setError("enter a valid username");
             valid = false;
         } else {
-            _emailText.setError(null);
+            _usernameText.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty() || password.length() < 1 || password.length() > 10) {
+            _passwordText.setError("between 1 and 10 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
@@ -135,4 +201,91 @@ public class LoginActivity extends AppCompatActivity {
 
         return valid;
     }
+
+    // callback
+    AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+        @Override
+        public void onSuccess(CognitoUserSession cognitoUserSession, CognitoDevice device) {
+            Log.e(TAG, "Auth Success");
+            AppHelper.setCurrSession(cognitoUserSession);
+            AppHelper.newDevice(device);
+            closeWaitDialog();
+            onLoginSuccess();
+            /*pass username and password info to main activity task*/
+            exit();
+
+        }
+
+        @Override
+        public void getAuthenticationDetails(AuthenticationContinuation continuation, String username) {
+            closeWaitDialog();
+            Locale.setDefault(Locale.US);
+//            getUserAuthentication(authenticationContinuation, username);
+            if (validate()){
+                AuthenticationDetails authenticationDetails = new AuthenticationDetails(username, password, null);
+                continuation.setAuthenticationDetails(authenticationDetails);
+                continuation.continueTask();
+            }
+        }
+
+        @Override
+        public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+            closeWaitDialog();
+//            mfaAuth(multiFactorAuthenticationContinuation);
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            closeWaitDialog();
+//            TextView label = (TextView) findViewById(R.id.textViewUserIdMessage);
+//            label.setText("Sign-in failed");
+////            _passwordText.setBackground(getDrawable(R.drawable.text_border_error));
+//
+//            label = (TextView) findViewById(R.id.textViewUserIdMessage);
+//            label.setText("Sign-in failed");
+//            _usernameText.setBackground(getDrawable(R.drawable.text_border_error));
+            onLoginFailed();
+//            showDialogMessage("Sign-in failed", AppHelper.formatException(e));
+        }
+
+        @Override
+        public void authenticationChallenge(ChallengeContinuation continuation) {
+            /**
+             * For Custom authentication challenge, implement your logic to present challenge to the
+             * user and pass the user's responses to the continuation.
+             */
+            if ("NEW_PASSWORD_REQUIRED".equals(continuation.getChallengeName())) {
+                // This is the first sign-in attempt for an admin created user
+                newPasswordContinuation = (NewPasswordContinuation) continuation;
+                AppHelper.setUserAttributeForDisplayFirstLogIn(newPasswordContinuation.getCurrentUserAttributes(),
+                        newPasswordContinuation.getRequiredAttributes());
+                closeWaitDialog();
+//                firstTimeSignIn();
+            }
+        }
+    };
+
+    private void mfaAuth(MultiFactorAuthenticationContinuation continuation) {
+        multiFactorAuthenticationContinuation = continuation;
+        Intent mfaActivity = new Intent(this, MFAActivity.class);
+        mfaActivity.putExtra("mode", multiFactorAuthenticationContinuation.getParameters().getDeliveryMedium());
+        startActivityForResult(mfaActivity, 5);
+    }
+
+    private void exit() {
+        Intent intent = new Intent(this, MainActivity.class);
+        /*pass in username and password info to MainActivity*/
+        if (username == null) {
+            username = "";
+        }
+        if (password == null) {
+            password = "";
+        }
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
+
