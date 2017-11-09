2676c09f382ac2a93bd11dce0914ab34e4329b8e
Siping Li <sli358@wisc.edu>
2017-11-08 18:34:24 -0600

    updated UI
    working

--- AndroidShareHome/app/src/main/java/sharehome/com/androidsharehome2/AddPostTab1.java
+++ AndroidShareHome/app/src/main/java/sharehome/com/androidsharehome2/AddPostTab1.java
@@ -1,6 +1,5 @@
 package sharehome.com.androidsharehome2;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,25 +7,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import butterknife.BindView;
 
 public class AddPostTab1 extends Fragment {
     private static final String TAG = "AddPostTab1";
-    @BindView(R.id.goback) TextView _cancel;
+    private Button btnTEST;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_post_tab1,container,false);
-        _cancel.setOnClickListener(new View.OnClickListener() {
+        btnTEST = (Button) view.findViewById(R.id.submitPost);
 
+        btnTEST.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-               
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
             }
         });
+
         return view;
     }
 }
